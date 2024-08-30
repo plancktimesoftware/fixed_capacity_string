cb10af0a4933ceda994b557ac4e41670645de957
plancktimesoftware <pranavkumar1108@gmail.com>
2024-08-31 01:39:53 +0530

    Adding replace and swap methods. Fixing null-terminator issue in insert. Allowing basic_string param with custom allocator.

--- fixed_capacity_string.h
+++ fixed_capacity_string.h
@@ -16,7 +16,7 @@ public:
 	using const_reference = const _Elem&;
 	static constexpr size_t npos = static_cast<size_t>(-1);
 
-	static auto from_sized(const_pointer ptr, size_t count) -> fixed_capacity_string_base
+	static constexpr auto from_sized(const_pointer ptr, size_t count) -> fixed_capacity_string_base
 	{
 		fixed_capacity_string_base obj;
 		if (ptr == nullptr) return obj;
@@ -27,40 +27,41 @@ public:
 		return obj;
 	}
 
-	static auto from_raw(const_pointer ptr) -> fixed_capacity_string_base
+	static constexpr auto from_raw(const_pointer ptr) -> fixed_capacity_string_base
 	{
 		return from_sized(ptr, _Traits::length(ptr));
 	}
 
-	static auto from_sv(const std::basic_string_view<_Elem, _Traits>& sv)
+	static constexpr auto from_sv(const std::basic_string_view<_Elem, _Traits>& sv)
 		-> fixed_capacity_string_base
 	{
 		return from_sized(sv.data(), sv.size());
 	}
 
-	static auto from_str(const std::basic_string<_Elem, _Traits>& str)
+	template<class _Alloc = std::allocator<_Elem>>
+	static constexpr auto from_str(const std::basic_string<_Elem, _Traits, _Alloc>& str)
 		-> fixed_capacity_string_base
 	{
 		return from_sized(str.data(), str.size());
 	}
 
-	auto c_str() const -> const_pointer { return mArray.data(); }
+	constexpr auto c_str() const -> const_pointer { return mArray.data(); }
 
-	auto sv() const -> std::basic_string_view<_Elem, _Traits>
+	constexpr auto sv() const -> std::basic_string_view<_Elem, _Traits>
 	{
 		return std::basic_string_view<_Elem, _Traits>(mArray.data(), mSize);
 	}
 
-	auto alloc_str() const -> std::basic_string<_Elem, _Traits>
+	constexpr auto alloc_str() const -> std::basic_string<_Elem, _Traits>
 	{
 		return std::basic_string<_Elem, _Traits>(mArray.data(), mSize);
 	}
 
-	auto length() const -> size_t { return mSize; }
-	auto size() const -> size_t { return mSize; }
-	auto capacity() const -> size_t { return _Capacity; }
+	constexpr auto length() const -> size_t { return mSize; }
+	constexpr auto size() const -> size_t { return mSize; }
+	constexpr auto capacity() const -> size_t { return _Capacity; }
 
-	auto assign(const_pointer ptr, size_t count) -> fixed_capacity_string_base&
+	constexpr auto assign(const_pointer ptr, size_t count) -> fixed_capacity_string_base&
 	{
 		mSize = count > _Capacity ? _Capacity : count;
 		_Traits::copy(mArray.data(), ptr, mSize);
@@ -68,31 +69,32 @@ public:
 		return *this;
 	}
 
-	auto assign(const_pointer ptr) -> fixed_capacity_string_base&
+	constexpr auto assign(const_pointer ptr) -> fixed_capacity_string_base&
 	{
 		return assign(ptr, _Traits::length(ptr));
 	}
 
-	auto assign(const std::basic_string_view<_Elem, _Traits>& sv)
+	constexpr auto assign(const std::basic_string_view<_Elem, _Traits>& sv)
 		-> fixed_capacity_string_base&
 	{
 		return assign(sv.data(), sv.size());
 	}
 
-	auto assign(const std::basic_string<_Elem, _Traits>& str)
+	template<class _Alloc = std::allocator<_Elem>>
+	constexpr auto assign(const std::basic_string<_Elem, _Traits, _Alloc>& str)
 		-> fixed_capacity_string_base&
 	{
 		return assign(str.data(), str.size());
 	}
 
 	template<size_t _OtherCapacity>
-	auto assign(fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
+	constexpr auto assign(fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
 		-> fixed_capacity_string_base&
 	{
 		return assign(other.data(), other.size());
 	}
 
-	auto append(const_pointer ptr, size_t count) -> fixed_capacity_string_base&
+	constexpr auto append(const_pointer ptr, size_t count) -> fixed_capacity_string_base&
 	{
 		size_t oldSize = mSize;
 		mSize =
@@ -104,31 +106,32 @@ public:
 		return *this;
 	}
 
-	auto append(const_pointer ptr) -> fixed_capacity_string_base&
+	constexpr auto append(const_pointer ptr) -> fixed_capacity_string_base&
 	{
 		return append(ptr, _Traits::length(ptr));
 	}
 
-	auto append(const std::basic_string_view<_Elem, _Traits>& sv)
+	constexpr auto append(const std::basic_string_view<_Elem, _Traits>& sv)
 		-> fixed_capacity_string_base&
 	{
 		return append(sv.data(), sv.size());
 	}
 
-	auto append(const std::basic_string<_Elem, _Traits>& str)
+	template<class _Alloc = std::allocator<_Elem>>
+	constexpr auto append(const std::basic_string<_Elem, _Traits, _Alloc>& str)
 		-> fixed_capacity_string_base&
 	{
 		return append(str.data(), str.size());
 	}
 
 	template<size_t _OtherCapacity>
-	auto append(fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
+	constexpr auto append(fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
 		-> fixed_capacity_string_base&
 	{
 		return append(other.data(), other.size());
 	}
 
-	auto insert(size_t index, const_pointer ptr, size_t count)
+	constexpr auto insert(size_t index, const_pointer ptr, size_t count)
 		-> fixed_capacity_string_base&
 	{
 		if (index >= mSize)
@@ -142,46 +145,45 @@ public:
 		_Elem* const insertAt = mArray.data() + index;
 		if (count < _Capacity - index)
 		{
-			// Inserted range will not exceed capacity. So we need to move the suffix to the end.
-			const size_t numElemsToMove = mSize - (index + count) + 1; // +1 to include the null-terminator.
+			// Inserted range will not exceed capacity.
+			// So we need to move the suffix to the end.
+			const size_t numElemsToMove = mSize - (index + count);
 			_Traits::move(insertAt + count, insertAt, numElemsToMove);
 		}
-		else
-		{
-			// Inserted range will exceed capacity. Just add a null-terminator at the new end.
-			_Traits::assign(mArray[mSize], _Elem());
-		}
+
 		const size_t numElemsToInsert = std::min(count, mSize - index);
 		_Traits::copy(insertAt, ptr, numElemsToInsert);
+		_Traits::assign(mArray[mSize], _Elem());
 		return *this;
 	}
 
-	auto insert(size_t index, const_pointer ptr) -> fixed_capacity_string_base&
+	constexpr auto insert(size_t index, const_pointer ptr) -> fixed_capacity_string_base&
 	{
 		return insert(index, ptr, _Traits::length(ptr));
 	}
 
-	auto insert(size_t index, const std::basic_string_view<_Elem, _Traits>& sv)
+	constexpr auto insert(size_t index, const std::basic_string_view<_Elem, _Traits>& sv)
 		-> fixed_capacity_string_base&
 	{
 		return insert(index, sv.data(), sv.size());
 	}
 
-	auto insert(size_t index, const std::basic_string<_Elem, _Traits>& str)
+	template<class _Alloc = std::allocator<_Elem>>
+	constexpr auto insert(size_t index, const std::basic_string<_Elem, _Traits, _Alloc>& str)
 		-> fixed_capacity_string_base&
 	{
 		return insert(index, str.data(), str.size());
 	}
 
 	template<size_t _OtherCapacity>
-	auto insert(size_t index,
+	constexpr auto insert(size_t index,
 				fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
 				-> fixed_capacity_string_base&
 	{
-		return insert(insert, other.data(), other.size());
+		return insert(index, other.data(), other.size());
 	}
 
-	auto insert(size_t index, size_t count, _Elem elem)
+	constexpr auto insert(size_t index, size_t count, _Elem elem)
 		-> fixed_capacity_string_base&
 	{
 		const bool insertAtEnd = index >= mSize;
@@ -199,28 +201,23 @@ public:
 			// We are inserting in the middle of the string, and
 			// the inserted range will not exceed capacity.
 			// So we need to move the suffix to the end.
-			const size_t numElemsToMove = mSize - (realIndex + count) + 1; // +1 to include the null-terminator.
+			const size_t numElemsToMove = mSize - (realIndex + count);
 			_Traits::move(insertAt + count, insertAt, numElemsToMove);
 		}
-		else
-		{
-			// We are inserting at the end, or
-			// the inserted range will exceed capacity.
-			// Just add a null-terminator at the new end.
-			_Traits::assign(mArray[mSize], _Elem());
-		}
+
 		const size_t numElemsToInsert = std::min(count, mSize - realIndex);
 		_Traits::assign(insertAt, numElemsToInsert, elem);
+		_Traits::assign(mArray[mSize], _Elem());
 		return *this;
 	}
 
-	auto clear() -> void
+	constexpr auto clear() -> void
 	{
 		mSize = 0;
 		_Traits::assign(mArray[mSize], _Elem());
 	}
 
-	auto erase(size_t index = 0, size_t count = npos)
+	constexpr auto erase(size_t index = 0, size_t count = npos)
 		-> fixed_capacity_string_base&
 	{
 		if (index >= mSize)
@@ -242,13 +239,79 @@ public:
 		return *this;
 	}
 
-	fixed_capacity_string_base() = default;
-	fixed_capacity_string_base(fixed_capacity_string_base& other) = default;
-	fixed_capacity_string_base(fixed_capacity_string_base&& other) = default;
-	auto operator=(fixed_capacity_string_base& other)->fixed_capacity_string_base & = default;
-	auto operator=(fixed_capacity_string_base&& other)->fixed_capacity_string_base & = default;
+	constexpr auto replace(size_t index, size_t count, const_pointer ptr, size_t count2)
+		-> fixed_capacity_string_base&
+	{
+		if (index >= mSize)
+			return append(ptr, count2);
 
-	auto data() -> pointer
+		if (count > mSize - index)
+			count = mSize - index;
+
+		_Elem* const replaceAt = mArray.data() + index;
+
+		if (count == count2)
+		{
+			_Traits::copy(replaceAt, ptr, count2);
+			return *this;
+		}
+
+		mSize = std::min(mSize + (count2 - count), _Capacity);
+		
+		if (count2 < _Capacity - index)
+		{
+			// Replaced range will not exceed capacity. So we need to move the suffix.
+			const size_t numElemsToMove = mSize - (index + count2);
+			_Traits::move(replaceAt + count2, replaceAt + count, numElemsToMove);
+		}
+
+		const size_t numElemsToInsert = std::min(count2, mSize - index);
+		_Traits::copy(replaceAt, ptr, numElemsToInsert);
+		_Traits::assign(mArray[mSize], _Elem());
+		return *this;
+	}
+
+	constexpr auto replace(size_t index, size_t count, const_pointer ptr)
+		-> fixed_capacity_string_base&
+	{
+		return replace(index, count, ptr, _Traits::length(ptr));
+	}
+
+	constexpr auto replace(size_t index, size_t count, const std::basic_string_view<_Elem, _Traits>& sv)
+		-> fixed_capacity_string_base&
+	{
+		return replace(index, count, sv.data(), sv.size());
+	}
+
+	template<class _Alloc = std::allocator<_Elem>>
+	constexpr auto replace(size_t index, size_t count,
+				 const std::basic_string<_Elem, _Traits, _Alloc>& str)
+				 -> fixed_capacity_string_base&
+	{
+		return replace(index, count, str.data(), str.size());
+	}
+
+	template<size_t _OtherCapacity>
+	constexpr auto replace(size_t index, size_t count,
+		fixed_capacity_string_base<_OtherCapacity, _Elem, _Traits>& other)
+		-> fixed_capacity_string_base&
+	{
+		return replace(index, count, other.data(), other.size());
+	}
+
+	constexpr auto swap(fixed_capacity_string_base& other) -> void
+	{
+		mArray.swap(other.mArray);
+		std::swap(mSize, other.mSize);
+	}
+
+	constexpr fixed_capacity_string_base() = default;
+	constexpr fixed_capacity_string_base(fixed_capacity_string_base& other) = default;
+	constexpr fixed_capacity_string_base(fixed_capacity_string_base&& other) = default;
+	constexpr auto operator=(fixed_capacity_string_base& other)->fixed_capacity_string_base & = default;
+	constexpr auto operator=(fixed_capacity_string_base&& other)->fixed_capacity_string_base & = default;
+
+	constexpr auto data() -> pointer
 	{
 		return mArray.data();
 	}
@@ -260,3 +323,16 @@ private:
 
 template<size_t _Capacity>
 using fixed_capacity_string = fixed_capacity_string_base<_Capacity, char>;
+
+#if __cpp_lib_char8_t
+template<size_t _Capacity>
+using fixed_capacity_u8string = fixed_capacity_string_base<_Capacity, char8_t>;
+#endif //__cpp_char8_t
+
+template<size_t _Capacity>
+using fixed_capacity_u16string = fixed_capacity_string_base<_Capacity, char16_t>;
+template<size_t _Capacity>
+using fixed_capacity_u32string = fixed_capacity_string_base<_Capacity, char32_t>;
+template<size_t _Capacity>
+using fixed_capacity_wstring = fixed_capacity_string_base<_Capacity, wchar_t>;
+
